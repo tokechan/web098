680dca03c558962cd7a647638eb08d9022379123
tokec <fleatoke@gmail.com>
2025-11-15 22:07:42 +0900

    Refactor layout structure to consolidate document rendering
    - Replaced the previous Layout component with a new Document component that encapsulates the entire HTML document structure.
    - Enhanced the Document component to include meta tags, favicon, and linked stylesheets for improved SEO and styling.
    - Updated the renderer to utilize the new Document component, streamlining the rendering process and ensuring consistency across the application.

--- second-ver/app/routes/_renderer.tsx
+++ second-ver/app/routes/_renderer.tsx
@@ -1,31 +1,6 @@
 import { jsxRenderer } from 'hono/jsx-renderer';
-import { Link, Script } from 'honox/server';
-import { Style } from 'hono/css';
-import { Layout } from '../components/templates/Layout';
+import Document from '../Layout';
 
-
-
-export default jsxRenderer(({ children, title }) => {
-  return (
-    <html lang="ja">
-      <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>{title}</title>
-        <link rel="icon" href="/favicon.ico" />
-        {/* CSS をheadにリンクで載せる */}
-        <link rel="stylesheet" href="/app/style.css" />
-        <link rel="stylesheet" href="/app/styles/global.css" />
-
-
-
-        <Script src="/app/client.ts" async />
-        <Style />{/* hono/cssの出力　*/}
-      
-      </head>
-      <body>
-        <Layout>{children}</Layout>
-      </body>
-    </html>
-  );
-});
+export default jsxRenderer(({ children, title }) => (
+  <Document title={title}>{children}</Document>
+));
